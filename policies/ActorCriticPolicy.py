Fixed gradient leak

policies/ActorCriticPolicy.py
@@ -7,9 +7,9 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class ActorCritic(nn.Module):
     def __init__(self, state_dim, action_dim, actor_hidden_units=[128, 128],
-                 critic_hidden_units=[128, 128], critic='StateValueCritic'):
+                 critic_hidden_units=[128, 128], critic_name='StateValueCritic'):
         super(ActorCritic, self).__init__()
-        self.critic_name = critic
+        self.critic_name = critic_name
         actor_intermediary_layers = []
         for hidden_last, hidden in zip(actor_hidden_units[:-1], actor_hidden_units[1:]):
             actor_intermediary_layers.append(nn.Linear(hidden_last, hidden))
@@ -23,7 +23,8 @@ class ActorCritic(nn.Module):
             nn.Tanh()
         )
 
-        self.critic = getattr(policies.critics, critic)(state_dim, action_dim, critic_hidden_units)
+        logging.warning('using critic {}'.format(critic_name))
+        self.critic = getattr(policies.critics, critic_name)(state_dim, action_dim, critic_hidden_units)
 
     def forward(self):
         raise NotImplementedError

policy_samplers/AdaptiveGaussianSampler.py
@@ -1,17 +1,20 @@
 import torch
 from .base_sampler import BaseSampler, matrix_diag
 import logging
+
 device = torch.device("cpu")
+
 
 class AdaptiveGaussianSampler(BaseSampler):
 
     def __init__(self, args_for_parse):
         pass
+
     def sample_action(self, actions):
         action_var = actions[:, actions.shape[1]//2:]/2 + (0.5 + 1e-7)
         action_mean = actions[:, :actions.shape[1]//2]
         covariance = matrix_diag(action_var)
-        # logging.info('Sampler covariance matrix: {}'.format(covariance))
+        logging.info('Sampler covariance matrix: {}'.format(covariance))
 
         dist = torch.distributions.MultivariateNormal(action_mean, covariance)
         action = dist.sample()
@@ -19,7 +22,7 @@ class AdaptiveGaussianSampler(BaseSampler):
         return action, action_logprobs
 
     def get_logprobs(self, actions, samples):
-        action_var = actions[:, actions.shape[1] // 2:]
+        action_var = actions[:, actions.shape[1] // 2:]/2 + (0.5 + 1e-7)
         action_mean = actions[:, :actions.shape[1] // 2]
 
         dist = torch.distributions.MultivariateNormal(action_mean, matrix_diag(action_var))